896fe3691566551231eb064e40dd1a9ca1cd4103
JonHunt1995 <116927012+JonHunt1995@users.noreply.github.com>
2026-03-17 20:22:28 -0500

    fix: added base to vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/battleshipfrontend/",
   plugins: [react()],
   server: {
     // This is the magic part
