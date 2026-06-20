5b9ecf397bc961ac423b5ac1c71dce3e548dedb3
JonHunt1995 <jnthnhunt495@gmail.com>
2026-06-20 10:09:52 -0500

    configured vite for docker

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,15 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/battleshipfrontend/",
-  plugins: [react()],
-  server: {
-    // This is the magic part
-    proxy: {
-      "/api": {
-        target: "http://localhost:4000", // Your Go API address
-        changeOrigin: true,
-      },
+    base: "/battleshipfrontend/",
+    plugins: [react()],
+    server: {
+        // This is the magic part
+        proxy: {
+            "/api": {
+                target: "'http://host.docker.internal:4000'", // Your Go API address
+                changeOrigin: true,
+            },
+        },
     },
-  },
 });
