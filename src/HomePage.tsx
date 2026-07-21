bc05ed6da2a3b0558dd3655b9d26778ca3383315
Jonathan Hunt <jonathanhunt@Jonathans-MacBook-Neo.local>
2026-07-21 13:42:21 -0500

    added https to qr code link

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -52,8 +52,8 @@ const HomePage = () => {
                 }} className="join-btn">
                 Join Game
             </button>
+            <ReactQRCode id="qr-code" value={link.startsWith('http') ? link : `https://${link}} />
           </div>
-          <ReactQRCode value={link.startsWith('http') ? link : `https://${link}} />
         </div>
         )}
         </section>
